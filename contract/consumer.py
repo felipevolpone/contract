a35125656911f0dff46476963f3551cb32872258
Felipe Volpone <felipevolpone@gmail.com>
2018-11-06 21:51:35 -0200

    Creating Consumer DSL

--- contract/consumer.py
+++ contract/consumer.py
@@ -1,3 +1,68 @@
+import json
 
-class Consumer:
-    x = 1
+
+class PropertieableJson:
+    def __init__(self, *args, **kwargs):
+        for property, value in kwargs.items():
+            setattr(self, property, value)
+
+    def to_json(self):
+        return vars(self)
+
+
+class InteractionToJson(PropertieableJson):
+    pass
+
+
+class RequestToJson(PropertieableJson):
+    pass
+
+
+class ResponseToJson(PropertieableJson):
+    pass
+
+
+
+class I:
+
+    def __init__(self, consumer_name):
+        self.consumer_name = consumer_name
+
+    def depends_on(self, provider_name):
+        self.provider_name = provider_name
+        return self
+
+    def to(self, description):
+        self.interaction_description = description
+        return self
+
+    def when(self, provider_state):
+        self.interaction_provider_state = provider_state
+        return self
+
+    def with_request(self, method, path, headers=None):
+        self.request_method = method
+        self.request_path = path
+        self.request_headers = headers
+        return self
+
+    def expecting_response(self, response_status, body={}):
+        self.response_status = response_status
+        self.response_body = body
+        return self
+
+    def done(self):
+        interaction = InteractionToJson(provider_state=self.interaction_provider_state,
+                                        description=self.interaction_description)
+        request = RequestToJson(method=self.request_method, path=self.request_path, headers=self.request_headers)
+        response = ResponseToJson(status=self.response_status, body=self.response_body)
+
+        interaction_json = interaction.to_json()
+        interaction_json['request'] = request.to_json()
+        interaction_json['response'] = response.to_json()
+        self._as_json = {'interactions': [interaction_json], 'consumer': {'name': self.consumer_name},
+                         'provider': {'name': self.provider_name}}
+        return self._as_json
+
+    def to_json(self):
+        return json.dumps(self._as_json)
--- tests/test_consumer.py
+++ tests/test_consumer.py
@@ -1,6 +1,26 @@
+import json
+from pprint import pprint
+from contract.consumer import I
 
-from contract.consumer import Consumer
 
+def test_consumer_json_generation():
+    consumer = (I("order-split-service").
+              depends_on("orders-api").
+              to("get a seller order").
+              when("seller order exists").
+              with_request('get', '/v1/seller-orders/WMT9CE111CDAED0/',
+                           headers={"Authorization": "Token xpto"}).
+              expecting_response(200, body={}))
 
-def test_consumer():
-    assert Consumer.x == 1
+    result = consumer.done()
+
+    assert result is not None
+    expected = {'consumer': {'name': 'order-split-service'},
+                'interactions': [{'description': 'get a seller order',
+                                  'provider_state': 'seller order exists',
+                                  'request': {'headers': {'Authorization': 'Token xpto'},
+                                                    'method': 'get',
+                                                    'path': '/v1/seller-orders/WMT9CE111CDAED0/'},
+                                  'response': {'body': {}, 'status': 200}}],
+                'provider': {'name': 'orders-api'}}
+    assert result == expected
